a58b98bf5de9d33c35345c7c990e0066571b1f33
Rhuanlps <rhuanfagundes07@gmail.com>
2024-08-21 20:52:27 -0300

    Criação do projeto java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer idadePessoa = 20;
+        String nomePessoa = "Claudin";
+
+        //    Operadores Logicos
+        // >  Maior
+        // >= Maoir igual
+        // <  Menor
+        // <= Menor igual
+        // == é igual
+        // != é diferente
+        // equal compara textos
+
+        if(idadePessoa > 21){
+            System.out.println("é maior que 21");
+        }else{
+            System.out.println("Não é maior");
+        }
+
     }
 }
